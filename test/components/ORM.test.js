ff4dea8073ade3ff5936eb2557b3d4b5f373d8f2
Loïc Blablaa <gamer204@gmail.com>
2014-01-16 22:39:56 +0100

    Updated: ORM.test.js

--- test/components/ORM.test.js
+++ test/components/ORM.test.js
@@ -11,11 +11,16 @@ describe("Components", function(){
 
 	describe("#ORM", function(){
 
-		it('should load models', function() {
-			ORM.models.should.haveproperty("User");
+		it('should connect to the database', function(){
+			ORM.connection.options.should.be.an.Object;
+			ORM.connection.options.dialect.should.be.eql("mysql");
+			ORM.connection.options.port.should.be.eql("3306");
 		});
 
-		it('should connect to the database');
+		it('should load models', function() {
+			ORM.models.should.have.property("User");
+			ORM.should.have.property("User");
+		});
 
 		it('should manage transactions');
 
