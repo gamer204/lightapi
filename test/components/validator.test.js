fcb7c753fcf1f6dc049f2cae1499de4554179e52
Loic Blablaa <gamer204@gmail.com>
2014-02-25 17:54:07 +0100

    Writing tests

--- test/components/validator.test.js
+++ test/components/validator.test.js
@@ -12,45 +12,11 @@ describe("Components", function(){
 
 	describe("#validator", function(){
 
-		beforeEach(function(){
-			schema = {
-				title: "!isNull",
-				message: {
-					"!isNull": null,
-					isLength: 10
-				},
-				category: {
-					isIn: [["cat1", "cat2", "cat3", "cat4"]]
-				}
-			};
-		})
+		it("should validate objects");
 
-		it('should return a valid object containing validation errors (or not)', function() {
-			var values = {
-				title: "Title",
-				message: "Correct message",
-				category: "cat4"
-			}
+		it("should sanitize objects");
 
-			validator(schema, values).should.be.an.Object.and.be.eql({});
-
-			values.category = "invalidCategory";
-			validator(schema, values).should.be.an.Object.and.have.a.property("category")
-				.with.properties("assert", "parameters")
-				.with.property("assert").eql("isIn");
-		});
-
-		it('should validate using `validator` methods', function() {
-			var values = {
-				title: "title",
-				message: "Too short",
-				category: "cat2"
-			}
-
-			var err = validator(schema, values);
-
-			node_val.isLength(values.message, 10).should.be.eql(!err.message);
-		});
+		it("should do both validate and sanitize");
 
 	});
 
