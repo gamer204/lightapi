b7e463386016b4fbbc52b4fa85bb6a28cc13ae0a
Loic Blablaa <gamer204@gmail.com>
2014-03-17 18:23:22 +0100

    Updated validator test

--- test/components/validator.test.js
+++ test/components/validator.test.js
@@ -75,11 +75,11 @@ describe("Components", function(){
 		it("should validate objects", function () {
 			delete schema.sanitize;
 
-			var result = la.validator(schema, candidate);
+			var result = validator(schema, candidate);
 			result.values.should.be.eql(candidate);
 
 			delete candidate.age;
-			result = la.validator(schema, candidate);
+			result = validator(schema, candidate);
 			result.error.should.have.property("error").with.be.an.Array.and.have.lengthOf(1);
 
 		});
@@ -87,32 +87,32 @@ describe("Components", function(){
 		it("should sanitize objects", function () {
 			delete schema.validation;
 
-			var result = la.validator(schema, candidate);
+			var result = validator(schema, candidate);
 			result.values.should.not.be.eql(candidate);
 			result.values.name.should.be.eql("François Le Français");
 			result.error.valid.should.be.true;
 
 			delete candidate.age;
-			result = la.validator(schema, candidate);
+			result = validator(schema, candidate);
 			result.error.valid.should.be.true;
 
 		});
 
 		it("should do both validate and sanitize", function () {
-			var result = la.validator(schema, candidate);
+			var result = validator(schema, candidate);
 			result.values.should.not.be.eql(candidate);
 			result.values.name.should.be.eql("François Le Français");
 			result.error.should.have.keys("error", "valid", "format");
 
 			delete candidate.age;
-			result = la.validator(schema, candidate);
+			result = validator(schema, candidate);
 			result.error.should.have.property("error").with.be.an.Array;
 
 		});
 
 		it("should throw an error if the schema has a bad syntax", function() {
 			(function(){
-				var result = la.validator({type: "object", properties: {bla: "blah"}}, candidate);
+				var result = validator({type: "object", properties: {bla: "blah"}}, candidate);
 			}).should.throw("Invalid schema syntax.");
 
 		});
