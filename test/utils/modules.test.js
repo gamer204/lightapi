c6ed610800ae748be2898a0264847fababb29656
Loïc Blablaa <gamer204@gmail.com>
2014-04-24 20:39:26 +0200

    Forgot a test

--- test/utils/modules.test.js
+++ test/utils/modules.test.js
@@ -3,17 +3,34 @@ describe("Utils", function(){
 	describe("#modules", function(){
 
 		it('should list modules', function (done) {
+
 			la.utils.modules.listModules(function(err, pkgs) {
 				if(err) {
 					throw err;
 				}
 
 				pkgs.should.be.an.Object;
-				pkgs.dependencies.should.have.property("lightapi");
+				pkgs.should.have.property("lightapi");
 
 				done();
 
 			});
+
+		});
+
+		it("should tell if a module is installed", function (done) {
+
+			la.utils.modules.hasModule("lightapi", function(err, res) {
+
+				if(err) throw err;
+
+				res.should.be.an.Object;
+				res.should.have.property("version");
+
+				done();
+
+			});
+
 		});
 
 	});
